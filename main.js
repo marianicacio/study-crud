700c8b1a35ea1ed8218f28efaac6cb41310d0306
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2025-02-04 15:45:47 -0300

    feat: new palete colors

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+//Eventos 
+
 document.getElementById('cadastrar')
     .addEventListener('click', openModal)
 
